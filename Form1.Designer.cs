df15a14275adb8a405df1ff3f9692778151b0fbd
Chongqinxcc <38058981+Chongqinxcc@users.noreply.github.com>
2018-12-12 16:26:33 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace ReadMysql
+{
+    partial class City
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cityBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.worldDataSet = new ReadMysql.worldDataSet();
+            this.worldDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cityTableAdapter = new ReadMysql.worldDataSetTableAdapters.cityTableAdapter();
+            this.cityBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cityBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.worldDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.worldDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cityBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy-MM-dd";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(84, 44);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(121, 21);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "yyyy-MM-dd";
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(84, 93);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(121, 21);
+            this.dateTimePicker2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "开始日期";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "结束日期";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(8, 140);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(54, 16);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "来源1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(7, 291);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(72, 16);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "城市名称";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(8, 242);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(54, 16);
+            this.checkBox3.TabIndex = 11;
+            this.checkBox3.Text = "来源3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(8, 193);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(54, 16);
+            this.checkBox4.TabIndex = 12;
+            this.checkBox4.Text = "来源2";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 21);
+            this.textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(84, 191);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 21);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(85, 240);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(120, 21);
+            this.textBox3.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(85, 289);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(120, 21);
+            this.textBox4.TabIndex = 16;
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.Location = new System.Drawing.Point(232, 26);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.RowTemplate.Height = 23;
+            this.DataGridView1.Size = new System.Drawing.Size(845, 259);
+            this.DataGridView1.TabIndex = 17;
+            // 
+            // cityBindingSource
+            // 
+            this.cityBindingSource.DataMember = "city";
+            this.cityBindingSource.DataSource = this.worldDataSet;
+            // 
+            // worldDataSet
+            // 
+            this.worldDataSet.DataSetName = "worldDataSet";
+            this.worldDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // worldDataSetBindingSource
+            // 
+            this.worldDataSetBindingSource.DataSource = this.worldDataSet;
+            this.worldDataSetBindingSource.Position = 0;
+            // 
+            // cityTableAdapter
+            // 
+            this.cityTableAdapter.ClearBeforeFill = true;
+            // 
+            // cityBindingSource1
+            // 
+            this.cityBindingSource1.DataMember = "city";
+            this.cityBindingSource1.DataSource = this.worldDataSet;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(232, 291);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "显示数据";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(437, 289);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "插入数据";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(583, 290);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "查询数据表";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(728, 289);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "删除数据";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // City
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 553);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DataGridView1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "City";
+            this.Text = "城市分类";
+            this.Load += new System.EventHandler(this.City_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cityBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.worldDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.worldDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cityBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataGridView DataGridView1;
+        private System.Windows.Forms.BindingSource worldDataSetBindingSource;
+        private worldDataSet worldDataSet;
+        private System.Windows.Forms.BindingSource cityBindingSource;
+        private worldDataSetTableAdapters.cityTableAdapter cityTableAdapter;
+        private System.Windows.Forms.BindingSource cityBindingSource1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;//pp
+
+
+
+namespace ReadMysql
+{
+    public partial class City : Form
+    {
+
+        private MySqlDataAdapter daCountry;//pp
+        private DataSet dsCountry;//pp
+
+        MySqlConnection con = new MySqlConnection("server=127.0.0.1;port=3306;user=root;password=CQxcc520*xcc; database=school;");
+
+        public City()
+        {
+            InitializeComponent();
+        }
+
+        private void City_Load(object sender, EventArgs e)
+        {
+            // TODO: 这行代码将数据加载到表“worldDataSet.city”中。您可以根据需要移动或删除它。
+            this.cityTableAdapter.Fill(this.worldDataSet.city);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("hhh");
+            
+
+            //String connetStr = "server=127.0.0.1;port=3306;user=root;password=CQxcc520*xcc; database=world;";
+            // server=127.0.0.1/localhost 代表本机，端口号port默认是3306可以不写
+            //MySqlConnection conn = new MySqlConnection(connetStr);
+            try
+            {
+                //conn.Open();//打开通道，建立连接，可能出现异常,使用try catch语句
+               con.Open();
+               Console.WriteLine("已经建立连接");
+                /* mDataAdapter = new MySqlDataAdapter("select *from city1", conn);
+                 dsall = new DataSet();
+                 mDataAdapter.Fill(dsall, "city1");
+                 Citydata1.DataSource = dsall;*/
+
+                //2018-12-4pp
+
+                //string sql = "SELECT Code, Name, HeadOfState FROM Country WHERE Region='Eastern Asia'";
+                string sql = "SELECT * FROM student";
+
+
+                //When a MySqlDataAdapter object is created, it is typically given an initial SELECT statement.
+                //daCountry = new MySqlDataAdapter(sql, conn);//创建一个接口，这个接口只接入Code, Name, HeadOfState三个字段，并且满足东亚的条件；
+                daCountry = new MySqlDataAdapter(sql, con);
+                /*
+                 * From this SELECT statement the Command Builder can work out the corresponding
+                 * INSERT, UPDATE and DELETE statements that would be required to update the database.
+                 */
+                MySqlCommandBuilder cb = new MySqlCommandBuilder(daCountry);//实现对从数据接口Adapter对数据进行插入、更新、删除操作；
+
+
+                dsCountry = new DataSet();
+                //Country是数据表
+                daCountry.Fill(dsCountry, "Country");//将Country数据表中的几个字段的数据读入数据集中；
+                DataGridView1.DataSource = dsCountry;//将数据集作为显示的输入源；
+                DataGridView1.DataMember = "Country";//获取更改的元素；使得程序可以更改显示的数据，留着以后更新到数据库中；
+
+            
+
+                //在这里使用代码对数据库进行增删查改
+
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //String connetStr1 = "server=127.0.0.1;port=3306;user=root;password=CQxcc520*xcc; database=school;";
+
+                //MySqlConnection con = null;
+                //con = new MySqlConnection(connetStr1);
+                con.Open();
+                if(con.State.ToString()=="Open")
+                {
+                    Console.WriteLine("成功连接MySQL");
+                    MySqlCommand com = new MySqlCommand();
+                    com.Connection = con; //获取连接
+                    //获取需要执行的INSERT语句
+                    com.CommandText = "INSERT INTO `score` (`id`, `stu_id`, `c_name`, `grade`) VALUES ('13', '906', '中文', '88'),('14', '907', '数学', '88')";
+                    //插入的记录数
+                    int i = com.ExecuteNonQuery();
+                    if (i > 0)
+                    {
+                        Console.WriteLine("插入的记录数为：" + i);
+                        i = 0;
+                    }
+                    
+                }
+                con.Close();
+
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
