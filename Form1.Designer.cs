75d928d89ba3bb7e736033013e659c0b127eaf4e
Chongqinxcc <38058981+Chongqinxcc@users.noreply.github.com>
2018-12-17 17:34:05 +0800

    复选框
    gridview增加了复选框，全选 取消全选功能

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -51,6 +51,8 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.selectAllbutton = new System.Windows.Forms.Button();
+            this.cancelAllbutton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.cityBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.worldDataSet)).BeginInit();
@@ -170,6 +172,8 @@
             this.DataGridView1.RowTemplate.Height = 23;
             this.DataGridView1.Size = new System.Drawing.Size(845, 259);
             this.DataGridView1.TabIndex = 17;
+            this.DataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellClick);
+            this.DataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
             // 
             // cityBindingSource
             // 
@@ -197,7 +201,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(232, 291);
+            this.button1.Location = new System.Drawing.Point(477, 293);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(161, 23);
             this.button1.TabIndex = 18;
@@ -207,7 +211,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(437, 289);
+            this.button2.Location = new System.Drawing.Point(661, 289);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 19;
@@ -217,7 +221,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(583, 290);
+            this.button3.Location = new System.Drawing.Point(752, 292);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 20;
@@ -226,18 +230,40 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(728, 289);
+            this.button4.Location = new System.Drawing.Point(897, 291);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 21;
             this.button4.Text = "删除数据";
             this.button4.UseVisualStyleBackColor = true;
             // 
+            // selectAllbutton
+            // 
+            this.selectAllbutton.Location = new System.Drawing.Point(232, 289);
+            this.selectAllbutton.Name = "selectAllbutton";
+            this.selectAllbutton.Size = new System.Drawing.Size(75, 23);
+            this.selectAllbutton.TabIndex = 22;
+            this.selectAllbutton.Text = "全选";
+            this.selectAllbutton.UseVisualStyleBackColor = true;
+            this.selectAllbutton.Click += new System.EventHandler(this.selectAllbutton_Click);
+            // 
+            // cancelAllbutton
+            // 
+            this.cancelAllbutton.Location = new System.Drawing.Point(314, 286);
+            this.cancelAllbutton.Name = "cancelAllbutton";
+            this.cancelAllbutton.Size = new System.Drawing.Size(75, 23);
+            this.cancelAllbutton.TabIndex = 23;
+            this.cancelAllbutton.Text = "取消全选";
+            this.cancelAllbutton.UseVisualStyleBackColor = true;
+            this.cancelAllbutton.Click += new System.EventHandler(this.cancelAllbutton_Click);
+            // 
             // City
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1101, 553);
+            this.Controls.Add(this.cancelAllbutton);
+            this.Controls.Add(this.selectAllbutton);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -292,6 +318,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button selectAllbutton;
+        private System.Windows.Forms.Button cancelAllbutton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -36,11 +36,27 @@ namespace ReadMysql
         private void button1_Click(object sender, EventArgs e)
         {
             Console.WriteLine("hhh");
-            
+
 
             //String connetStr = "server=127.0.0.1;port=3306;user=root;password=CQxcc520*xcc; database=world;";
             // server=127.0.0.1/localhost 代表本机，端口号port默认是3306可以不写
             //MySqlConnection conn = new MySqlConnection(connetStr);
+
+            //第一列添加复选框
+            DataGridViewCheckBoxColumn dtchk = new DataGridViewCheckBoxColumn();
+            // dtchk.DataPropertyName = "chk";
+            dtchk.Name = "chk";
+            dtchk.HeaderText = "全选";
+
+            //DataGridView1.ColumnHeadersVisible = false;//需要显示列标题
+            dtchk.TrueValue = true;
+            dtchk.FalseValue = false;
+            dtchk.DataPropertyName = "IsChecked";
+
+            DataGridView1.Columns.Add(dtchk);
+            DataGridView1.Columns[0].Width = 45;
+
+
             try
             {
                 //conn.Open();//打开通道，建立连接，可能出现异常,使用try catch语句
@@ -73,6 +89,8 @@ namespace ReadMysql
                 DataGridView1.DataSource = dsCountry;//将数据集作为显示的输入源；
                 DataGridView1.DataMember = "Country";//获取更改的元素；使得程序可以更改显示的数据，留着以后更新到数据库中；
 
+
+              
             
 
                 //在这里使用代码对数据库进行增删查改
@@ -125,6 +143,137 @@ namespace ReadMysql
             finally
             {
                 Console.ReadLine();
+            }
+        }
+
+        //选中单元格的内容时 处理事件
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            /*
+            if (e.ColumnIndex == 0 && e.RowIndex != -1)
+            {
+                DataGridViewCheckBoxCell checkCell = (DataGridViewCheckBoxCell)this.DataGridView1.Rows[e.RowIndex].Cells["chk"];
+                Boolean flag = Convert.ToBoolean(checkCell.Value);
+                if (flag == true)
+                {
+                    checkCell.Value = false;
+                    Console.WriteLine("没选中");
+                }
+                else
+                {
+                    checkCell.Value = true;
+                    Console.WriteLine("选中");
+                }
+            }
+            */
+            try
+            {
+                int index = DataGridView1.CurrentRow.Index;
+                this.DataGridView1.Rows[e.RowIndex].Selected = true;
+                if (Convert.ToBoolean(DataGridView1.Rows[index].Cells[0].Value))
+                {
+                    DataGridView1.Rows[index].Cells[0].Value = false;
+                }
+                else
+                {
+                    DataGridView1.Rows[index].Cells[0].Value = true;
+                    ////其他的都是false
+                    //for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                    //{
+                    //    if (i != index)
+                    //    {
+                    //        dataGridView1.Rows[i].Cells[0].Value = false;
+                    //    }
+                    //}
+                }
+            }
+            catch
+            {
+                Console.WriteLine("错误");
+            }
+
+        }
+        
+        //单击单元格的时候发生的事件
+        /// <summary>
+        /// 如果单击列表头，全选．但是小箭头在的一行不会被选中，不知道为啥
+        /// 暂时保留
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            /*
+            if (e.RowIndex == -1)//如果单击列表头，全选．
+            {
+                int i;
+                for (i = 0; i < this.DataGridView1.RowCount; i++)
+                {
+                    if ((Convert.ToBoolean(DataGridView1.Rows[i].Cells[0].Value) == false))
+                    {
+                        this.DataGridView1.Rows[i].Cells[0].Value = "true";//如果为true则为选中,false未选中
+                        Console.WriteLine("当前行"+i);
+                    }
+                        
+                }
+            }
+            */
+            try
+            {
+                int index = DataGridView1.CurrentRow.Index;
+                this.DataGridView1.Rows[e.RowIndex].Selected = true;
+                if (Convert.ToBoolean(DataGridView1.Rows[index].Cells[0].Value))
+                {
+                    DataGridView1.Rows[index].Cells[0].Value = false;
+                }
+                else
+                {
+                    DataGridView1.Rows[index].Cells[0].Value = true;
+                    ////其他的都是false
+                    //for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                    //{
+                    //    if (i != index)
+                    //    {
+                    //        dataGridView1.Rows[i].Cells[0].Value = false;
+                    //    }
+                    //}
+                }
+            }
+            catch
+            {
+                Console.WriteLine("错误");
+            }
+
+
+        }
+
+        // 全选
+
+        private void selectAllbutton_Click(object sender, EventArgs e)
+        {
+            //遍历datagridview中的每一行，判断是否选中，若为选中，则选中
+            for (int i = 0; i < DataGridView1.Rows.Count; i++)
+            {
+                if ((Convert.ToBoolean(DataGridView1.Rows[i].Cells[0].Value) == false))
+                {
+                    DataGridView1.Rows[i].Cells[0].Value = "True";
+                }
+                else
+                    continue;
+            }
+        }
+
+        ///取消全选
+        private void cancelAllbutton_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < DataGridView1.Rows.Count; i++)
+            {
+                if ((Convert.ToBoolean(DataGridView1.Rows[i].Cells[0].Value) == true))
+                {
+                    DataGridView1.Rows[i].Cells[0].Value = "False";
+                }
+                else
+                    continue;
             }
         }
     }
